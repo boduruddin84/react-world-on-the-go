2200ecbdf6e045eba5995131b135159c857d38d5
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-03-15 12:21:29 +0600

    Visited Button Functionality Added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import './country.css';
 
-const Country = () => {
+
+
+const Country = ({country}) => {
+
+    const [visited, setVisited] = useState(false);
+
+
+    const handleVisited = () => {
+    setVisited(true);
+    }
+
     return (
-        <div>
-            <h3>Name: </h3>
+        <div className="country">
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            <p>Capital: {country.capital.capital}</p>
+            <p>Region: {country.region.region}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 }
